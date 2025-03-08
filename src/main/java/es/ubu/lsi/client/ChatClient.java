319f979c041cd6b75ec04aee28d6c695e65f11bc
aperezolmos <amandaprzolmos@gmail.com>
2025-03-08 22:16:04 +0100

    Esqueleto general clases

---
+++ src/main/java/es/ubu/lsi/client/ChatClient.java
@@ -0,0 +1,12 @@
+package es.ubu.lsi.client;
+
+import es.ubu.lsi.common.ChatMessage;
+
+public interface ChatClient {
+
+	public boolean start();
+	
+	public void sendMessage(ChatMessage msg);
+	
+	public void disconnect();
+}
